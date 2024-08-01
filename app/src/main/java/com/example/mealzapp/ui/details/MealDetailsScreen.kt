8d87dcb399e69d67c519f56842e50b52e7500164
Savo Lekovic <savolekovic15@gmail.com>
2024-08-01 14:12:08 +0200

    adds image animation onClick

--- app/src/main/java/com/example/mealzapp/ui/details/MealDetailsScreen.kt
+++ app/src/main/java/com/example/mealzapp/ui/details/MealDetailsScreen.kt
@@ -1,12 +1,13 @@
 package com.example.mealzapp.ui.details
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -17,10 +18,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -38,6 +46,12 @@ fun MealDetailsScreen(
     val viewModel: MealDetailsViewModel = viewModel()
     val mealDetailsState = viewModel.mealDetailsState.value
 
+    var isExpanded by remember { mutableStateOf(false) }
+    val imageSizeDp: Dp by animateDpAsState(
+        targetValue = if (isExpanded) 200.dp else 100.dp,
+        label = "Animate Image"
+    )
+
     LaunchedEffect(key1 = true) {
         viewModel.getMealDetails(id)
     }
@@ -54,51 +68,51 @@ fun MealDetailsScreen(
             Box(
                 modifier = Modifier
                     .fillMaxSize()
+                    .padding(innerPadding)
             ) {
                 Card(
                     modifier = Modifier
-                        .align(Alignment.Center)
                         .padding(16.dp)
                 ) {
                     Column(
                         modifier = Modifier
                             .wrapContentSize()
-                            .padding(innerPadding),
+                            .padding(16.dp),
                     ) {
-
                         AsyncImage(
                             model = meal.imageUrl,
                             contentDescription = null,
                             contentScale = ContentScale.Crop,
                             modifier = Modifier
-                                .size(200.dp)
+                                .size(imageSizeDp)
                                 .padding(16.dp)
-                                .align(Alignment.CenterHorizontally)
                                 .clip(CircleShape)
+                                .background(Color.Blue)
+                                .align(Alignment.CenterHorizontally)
                         )
 
                         Text(
                             text = meal.name,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .wrapContentHeight()
-                                .padding(top = 8.dp, start = 12.dp, end = 12.dp)
-                                .align(Alignment.Start),
+                                .padding(top = 8.dp, start = 12.dp, end = 12.dp),
+                            textAlign = TextAlign.Center,
                             style = MaterialTheme.typography.titleLarge
                         )
                         Text(
                             text = meal.description,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .wrapContentHeight()
-                                .padding(top = 8.dp, start = 12.dp, end = 12.dp)
-                                .align(Alignment.Start),
+                                .padding(top = 8.dp, start = 12.dp, end = 12.dp),
+                            textAlign = TextAlign.Center,
                             style = MaterialTheme.typography.titleSmall
                         )
 
                         Button(
-                            onClick = { /*TODO*/ },
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                            onClick = { isExpanded = !isExpanded },
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(top = 8.dp)
                         ) {
                             Text(text = "Change state of meal profile picture")
                         }
